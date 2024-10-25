3e5e584b32caa5ff9353fa4137fe9026be97c275
urata-smakiqi <urataasmakiqi@gmail.com>
2024-10-25 15:34:09 +0200

    Update medical_diagnosis.py

--- medical_diagnosis.py
+++ medical_diagnosis.py
@@ -12,7 +12,7 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
-embeddings = OpenAIEmbeddings(openai.api_key=openai.api_key)
+embeddings = OpenAIEmbeddings(openai_api_key=openai.api_key)
 dimension = 1536
 index = faiss.IndexFlatL2(dimension)
 faiss_index = FAISS(embedding_function=embeddings.embed_query, index=index, docstore=None, index_to_docstore_id={})
