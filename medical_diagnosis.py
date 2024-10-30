1398cefc23b0a22b565521cc497fad7239ddc13e
urata-smakiqi <urataasmakiqi@gmail.com>
2024-10-30 14:57:11 +0100

    Update medical_diagnosis.py

--- medical_diagnosis.py
+++ medical_diagnosis.py
@@ -9,7 +9,7 @@ import os
 import openai
 import json
 load_dotenv()
-openai.api_key = os.getenv(OPENAI_API_KEY)
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 embeddings = OpenAIEmbeddings()
 dimension = 1536
